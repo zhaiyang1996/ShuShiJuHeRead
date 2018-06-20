06a78848d2c7d27767a853d4a6884f3baa367647
Administrator <806873918@qq.com>
2018-06-20 17:56:04 +0800

    书架和详情

---
+++ app/src/main/java/com/shushijuhe/shushijuheread/activity/BookrackActivity.java
@@ -0,0 +1,43 @@
+package com.shushijuhe.shushijuheread.activity;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+import com.shushijuhe.shushijuheread.R;
+import com.shushijuhe.shushijuheread.activity.base.BaseActivity;
+import com.shushijuhe.shushijuheread.adapter.BookrackAdapter;
+import com.shushijuhe.shushijuheread.utils.TopMenuHeader;
+
+import butterknife.BindView;
+
+/**
+ * 书架
+ */
+public class BookrackActivity extends BaseActivity {
+    @BindView(R.id.bookrack_rv_shelf)
+    RecyclerView shelf;//书架
+    private BookrackAdapter bookrackAdapter;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_bookrack;
+    }
+
+    @Override
+    public void initToolBar() {
+        // 顶部设置
+        TopMenuHeader topMenu = new TopMenuHeader(getWindow().getDecorView(), this);
+        topMenu.setTopMenuHeader(R.mipmap.title_backtrack, "",
+                "书架", R.mipmap.title_seek, R.mipmap.title_menu);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+        bookrackAdapter = new BookrackAdapter(this);
+    }
+}
