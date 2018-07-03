37f64dda880862ac46ef2e14893f33932024673f
Administrator <806873918@qq.com>
2018-07-03 09:37:06 +0800

    书架

--- app/src/main/java/com/shushijuhe/shushijuheread/activity/BookrackActivity.java
+++ app/src/main/java/com/shushijuhe/shushijuheread/activity/BookrackActivity.java
@@ -1,26 +1,36 @@
 package com.shushijuhe.shushijuheread.activity;
 
+import android.content.Intent;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.shushijuhe.shushijuheread.MainActivity;
 import com.shushijuhe.shushijuheread.R;
 import com.shushijuhe.shushijuheread.activity.base.BaseActivity;
 import com.shushijuhe.shushijuheread.adapter.BookrackAdapter;
-import com.shushijuhe.shushijuheread.bean.Book_infoBean;
-import com.shushijuhe.shushijuheread.http.DataManager;
-import com.shushijuhe.shushijuheread.http.ProgressSubscriber;
-import com.shushijuhe.shushijuheread.http.SubscriberOnNextListenerInstance;
+import com.shushijuhe.shushijuheread.bean.BookshelfBean;
+import com.shushijuhe.shushijuheread.dao.BookshelfBeanDaoUtils;
 import com.shushijuhe.shushijuheread.utils.TopMenuHeader;
+
+import java.util.List;
 
 import butterknife.BindView;
 
 /**
- * 书架
+ * 刘鹏
+ * 书架页面
  */
 public class BookrackActivity extends BaseActivity {
     @BindView(R.id.bookrack_rv_shelf)
-    RecyclerView shelf;//书架
+    RecyclerView mRecyclerView;//书架
     private BookrackAdapter bookrackAdapter;
+    private List<BookshelfBean> list;
+    private BookshelfBeanDaoUtils bookshelfBeanDaoUtils;
 
     @Override
     public int getLayoutId() {
@@ -33,6 +43,13 @@ public class BookrackActivity extends BaseActivity {
         TopMenuHeader topMenu = new TopMenuHeader(getWindow().getDecorView(), this);
         topMenu.setTopMenuHeader(R.mipmap.title_backtrack, "",
                 "书架", R.mipmap.title_seek, R.mipmap.title_menu);
+        topMenu.getTopIvRight().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookrackActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -43,5 +60,37 @@ public class BookrackActivity extends BaseActivity {
     @Override
     public void initEvent() {
         bookrackAdapter = new BookrackAdapter(this);
+        bookshelfBeanDaoUtils = new BookshelfBeanDaoUtils(this);
+        initData();
+        callBack();
+    }
+
+    /**
+     * 回调
+     */
+    private void callBack() {
+        bookrackAdapter.setOnItemClickListener(new BookrackAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BookshelfBean bookshelfBea, ImageView imageView) {
+                if(imageView!=null){
+                    Toast.makeText(mContext, "长按", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(mContext, "单击", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        list = bookshelfBeanDaoUtils.queryAllBookshelfBean();
+        bookshelfBeanDaoUtils.closeConnection();//关闭数据库
+        bookrackAdapter.setData(list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));//设置布局管理器
+        mRecyclerView.setAdapter(bookrackAdapter);//设置Adapter
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL)); //添加Android自带的分割线
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator()); //设置增加或删除条目的动画
     }
 }
--- app/src/main/java/com/shushijuhe/shushijuheread/adapter/BookrackAdapter.java
+++ app/src/main/java/com/shushijuhe/shushijuheread/adapter/BookrackAdapter.java
@@ -3,35 +3,102 @@ package com.shushijuhe.shushijuheread.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.shushijuhe.shushijuheread.R;
+import com.shushijuhe.shushijuheread.bean.BookshelfBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 书架适配器
  */
 public class BookrackAdapter extends RecyclerView.Adapter<BookrackAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<BookshelfBean> list;
+    private OnItemClickListener onItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(BookshelfBean bookshelfBea, ImageView imageView);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
     public BookrackAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<BookshelfBean> list) {
+        this.list = list;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_bookrack_shelf, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
+        holder.name.setText(String.valueOf(list.get(position).getName()));
+        holder.chapter.setText("最新章节：");
+        holder.time.setText(String.valueOf(list.get(position).getTime()));
+        Glide.with(context)
+                .load(list.get(position).getCover())
+                .into(holder.cover);
+        holder.mlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.onItemClick(list.get(position), null);
+            }
+        });
+        holder.mlayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                onItemClickListener.onItemClick(list.get(position), holder.selected);
+                return true;
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(View itemView) {
+        @BindView(R.id.item_bookrack_name)
+        TextView name;//书名
+        @BindView(R.id.item_bookrack_chapter)
+        TextView chapter;//章节
+        @BindView(R.id.item_bookrack_time)
+        TextView time;//时间
+        @BindView(R.id.item_bookrack_cover)
+        ImageView cover;//封面
+        @BindView(R.id.item_bookrack_selected)
+        ImageView selected;//是否选中
+        @BindView(R.id.item_bookrack_linearLayout)
+        LinearLayout mlayout;//item
+
+
+        MyViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
