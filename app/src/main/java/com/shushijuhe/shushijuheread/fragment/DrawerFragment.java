4d5fa8f5661c8c5a5bccce2f8c1d3a67af5a14b1
zhaiyang <http://git.jisheyun.com/JiShe.Infrared.Android.git>
2018-07-25 17:33:09 +0800

    修改阅读UI以及书架删除

--- app/src/main/java/com/shushijuhe/shushijuheread/fragment/DrawerFragment.java
+++ app/src/main/java/com/shushijuhe/shushijuheread/fragment/DrawerFragment.java
@@ -1,8 +1,17 @@
 package com.shushijuhe.shushijuheread.fragment;
 
-import com.shushijuhe.shushijuheread.R;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
-public class DrawerFragment extends BaseFragment {
+import com.shushijuhe.shushijuheread.R;
+import com.shushijuhe.shushijuheread.activity.AboutUsActivity;
+
+import butterknife.BindView;
+
+public class DrawerFragment extends BaseFragment implements View.OnClickListener{
+    @BindView(R.id.drawer_me)
+    TextView textView;
     @Override
     public int getLayoutId() {
         return R.layout.fragment_drawer;
@@ -20,6 +29,15 @@ public class DrawerFragment extends BaseFragment {
 
     @Override
     public void initEvent() {
+        textView.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.drawer_me:
+                startActivity(new Intent(getActivity(), AboutUsActivity.class));
+                break;
+        }
     }
 }
